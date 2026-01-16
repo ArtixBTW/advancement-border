dc97cffe71793ce674bb740f613487d77e1a564b
Lewis Arias <lewisgarias@outlook.com>
2026-01-15 20:41:46 -0500

    Port config to FzzyConfig (yes we changed libraries *again*)

--- src/main/java/net/reimaden/advancementborder/AdvancementBorder.java
+++ src/main/java/net/reimaden/advancementborder/AdvancementBorder.java
@@ -3,18 +3,19 @@ package net.reimaden.advancementborder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.wispforest.endec.Endec;
-import io.wispforest.endec.impl.ReflectiveEndecBuilder;
-import io.wispforest.owo.network.OwoNetChannel;
+import me.fzzyhmstrs.fzzy_config.api.ConfigApiJava;
+import me.fzzyhmstrs.fzzy_config.api.RegisterType;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.resources.Identifier;
 import net.minecraft.server.players.PlayerList;
 
 public final class AdvancementBorder implements ModInitializer {
     public static final String MOD_ID = "advancementborder";
     public static final String MOD_NAME = "Advancement Border";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static final AdvancementBorderConfig config = AdvancementBorderConfig.createAndLoad();
+    public static final AdvancementBorderConfig config = ConfigApiJava.registerAndLoadConfig(
+            AdvancementBorderConfig::new,
+            // TODO: make sure we don't override clients
+            RegisterType.BOTH);
 
     @Override
     public void onInitialize() {
---
+++ src/main/java/net/reimaden/advancementborder/AdvancementBorderConfig.java
@@ -0,0 +1,99 @@
+package net.reimaden.advancementborder;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+import me.fzzyhmstrs.fzzy_config.api.FileType;
+import me.fzzyhmstrs.fzzy_config.api.SaveType;
+import me.fzzyhmstrs.fzzy_config.config.Config;
+import me.fzzyhmstrs.fzzy_config.util.Walkable;
+import me.fzzyhmstrs.fzzy_config.validation.collection.ValidatedIdentifierMap;
+import me.fzzyhmstrs.fzzy_config.validation.minecraft.ValidatedIdentifier;
+import me.fzzyhmstrs.fzzy_config.validation.misc.ValidatedAny;
+import me.fzzyhmstrs.fzzy_config.validation.misc.ValidatedColor;
+import net.minecraft.resources.Identifier;
+
+public final class AdvancementBorderConfig extends Config {
+    private static final Identifier IDENTIFIER = Identifier.fromNamespaceAndPath(
+            AdvancementBorder.MOD_ID,
+            "server");
+
+    public AdvancementBorderConfig() {
+        super(IDENTIFIER);
+    }
+
+    @Override
+    public FileType fileType() {
+        return FileType.JSON5;
+    }
+
+    @Override
+    public SaveType saveType() {
+        return SaveType.SEPARATE;
+    }
+
+    public AdvancementsConfig advancements = new AdvancementsConfig();
+
+    public static class AdvancementsConfig implements Walkable {
+        /**
+         * Whether each player contributes separately instead of using a global list of
+         * advancements
+         */
+        public boolean perPlayer = false;
+
+        public BlacklistConfig blacklist = new BlacklistConfig();
+
+        public static class BlacklistConfig implements Walkable {
+            public boolean invert = false;
+
+            public List<Identifier> advancements = new ArrayList<>();
+        }
+    }
+
+    public ExpansionNotificationConfig expansionNotification = new ExpansionNotificationConfig();
+
+    public static enum NotificationLocation {
+        CHAT,
+        ACTION_BAR,
+        NONE,
+    }
+
+    public static class ExpansionNotificationConfig implements Walkable {
+        /** Whether to display the amount of blocks the world border expands */
+        public boolean showAmountOfBlocks = false;
+
+        /** Where to send notifications to */
+        public NotificationLocation location = NotificationLocation.CHAT;
+
+        /** Text color of world border notifications */
+        public ValidatedColor textColor = new ValidatedColor(Color.decode("#55FFFF"), false);
+    }
+
+    public DimensionConfigs dimensions = new DimensionConfigs();
+
+    public static class DimensionConfigs implements Walkable {
+        public DimensionConfig fallback = new DimensionConfig();
+
+        public ValidatedIdentifierMap<DimensionConfig> dimensions = new ValidatedIdentifierMap.Builder<DimensionConfig>()
+                .keyHandler(new ValidatedIdentifier(Identifier.withDefaultNamespace("overworld")))
+                .valueHandler(new ValidatedAny<DimensionConfig>(new DimensionConfig()))
+                .build();
+
+        public static class DimensionConfig implements Walkable {
+            public CenterConfig center = new CenterConfig();
+            public IncreaseAmountConfig increaseAmount = new IncreaseAmountConfig();
+
+            public static class CenterConfig implements Walkable {
+                public double x = 0.5;
+                public double z = 0.5;
+            }
+
+            public static class IncreaseAmountConfig implements Walkable {
+                public double task = 2.0;
+                public double goal = 2.0;
+                public double challenge = 2.0;
+            }
+        }
+    }
+}
